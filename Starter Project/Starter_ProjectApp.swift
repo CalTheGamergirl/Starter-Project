e5f1f03ec4bef94ca485e1627c1448d52e152233
CalTheGamerGirl <ecash.peterson@gmail.com>
2022-08-07 15:01:55 +0000

    Initial Commit

---
+++ Starter Project/Starter_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Starter_ProjectApp.swift
+//  Starter Project
+//
+//  Created by Cal Cash-Peterson on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Starter_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
