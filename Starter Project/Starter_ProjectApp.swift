cff28798da649d566c2380e16e41f382a9687200
CalTheGamerGirl <ecash.peterson@gmail.com>
2022-08-07 17:22:05 +0000

    This is 5 lessons of the https://developer.apple.com/tutorials/app-dev-training which i am following to create an app.

--- Starter Project/Starter_ProjectApp.swift
+++ Starter Project/Starter_ProjectApp.swift
@@ -1,17 +1,13 @@
-//
-//  Starter_ProjectApp.swift
-//  Starter Project
-//
-//  Created by Cal Cash-Peterson on 8/7/22.
-//
-
 import SwiftUI
 
 @main
 struct Starter_ProjectApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+            ScrumsView(scrums: DailyScrum.sampleData)
+        
+            }
         }
     }
 }
